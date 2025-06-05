0b49becb50fc660d65ac11caac8d470eec2c1036
rocopolas <86751781+rocopolas@users.noreply.github.com>
2025-06-05 14:48:11 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ session_start();
 require_once 'vendor/autoload.php';
 
 $google_client = new Google_Client();
-$google_client->setClientId('1040481639283-lnhcfu8dqa1fctbdks3c940mmj4mthjd.apps.googleusercontent.com');
-$google_client->setClientSecret('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');
+$google_client->setClientId('.apps.googleusercontent.com');
+$google_client->setClientSecret('');
 $google_client->setRedirectUri('http://localhost/index.php');
 
 $google_client->addScope('email');
